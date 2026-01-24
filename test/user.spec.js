0500ddfabad65590c1590242084d9199b3fd95a9
Cláudio Cardoso <claudio.c.lima@hotmail.com>
2026-01-24 20:20:13 -0300

    21. Deve listar os usuários

---
+++ test/user.spec.js
@@ -0,0 +1,13 @@
+const request = require('supertest')
+const app = require('../src/app')
+
+describe('User', () => {
+  it('Deve listar os usuários', async () => {
+    await request(app).get('/users')
+    .then((res) => {
+      expect(res.status).toBe(200)
+      expect(res.body).toHaveLength(1)
+      expect(res.body[0]).toHaveProperty('name', 'John Doe')
+    })
+  })
+})
